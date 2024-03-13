99b02f7b1e2cc82b22640bcd2a043c268eb68933
Fussbaguette <112727594+Fussbaguette@users.noreply.github.com>
2024-03-13 18:09:01 -0400

    Server Logic

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from flask import Response, make_response, request, send_from_directory
+from flask import Response, make_response, request, send_from_directory, redirect
 from pymongo import MongoClient
 import random
 import hashlib
@@ -27,26 +27,27 @@ def Cookiegen(x):
     for i in range(x):
         chars += (random.choice(string))
     return chars
-#Generate a Cookie
-def Cookiegen(x):
-    string = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    chars=""
-    for i in range(x):
-        chars += (random.choice(string))
-    return chars
-#Generate a Cookie
-def Cookiegen(x):
-    string = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-=_+{}[]|\:;?/<>,."
-    chars=""
-    for i in range(x):
-        chars += (random.choice(string))
-    return chars
 
 #when / is url returns index.html contents as home page and also calls on css/js files
 @app.route("/", methods=['GET'])
 def home():
-    
-    return render_template('index.html')    
+    if request.args.get('LoginMessage'):
+        return render_template('index.html', LoginMessage = request.args.get('LoginMessage'))
+    elif request.args.get('RegisterMessage'):
+        return render_template('index.html', RegisterMessage = request.args.get('RegisterMessage'))
+    elif request.args.get('LogOutMessage'):
+        return render_template('index.html',LogOutMessage = request.args.get('LogOutMessage')) 
+    else:
+        return render_template('index.html')
+ 
+# Battle Page Rendering
+@app.route("/battle", methods=['GET'])
+def BattlePage():
+    #Checks Cookie and Auth to if user exist
+    if 'auth' in request.cookies and userdata.find_one({"auth_token": hashlib.sha256((request.cookies.get('auth')).encode('utf-8')).hexdigest()}):
+        return render_template('battle.html', UserName = userdata.find_one({"auth_token": hashlib.sha256((request.cookies.get('auth')).encode('utf-8')).hexdigest()})['username']) 
+    else:
+        return redirect('/')
 
 #Register for an account
 @app.route("/register", methods=['POST'])
@@ -57,58 +58,58 @@ def register():
     pw_retype = request.form['password_reg_retype']
     #Check for Miss match password
     if pw != pw_retype:
-        return render_template('index.html', RegisterMessage="Password Miss Match")
+        return redirect(f'/?RegisterMessage={"Password Miss Match"}')
     #Check if username already in use
     if userdata.find_one({"username": username}):
-        return render_template('index.html', RegisterMessage="Username already in use")
+        return redirect(f'/?RegisterMessage={"Username already in use"}')
     #Generate user info
     salt = Saltgen(16)
     hashpass = hashlib.sha256((pw+salt).encode('utf-8')).hexdigest()
-    user_info = {"username" : username, "password": hashpass,"salt": salt, "aut_token": ''}
-    user_info = {"username" : username, "password": hashpass,"salt": salt, "aut_token": ''}
+    user_info = {"username" : username, "password": hashpass,"salt": salt, "auth_token": ''}
     #Insert in DB
     userdata.insert_one(user_info)
-    return render_template('index.html', RegisterMessage="Registration Successful")
+    return redirect(f'/?RegisterMessage={"Registration Successful"}')
 
 #Login
 @app.route("/login", methods=['POST'])
 def login():
-    #Get the credentials
+    #Get the credentials from form
     username = request.form['username_login']
     pw = request.form['password_login']
     #Search database
     if userdata.find_one({"username": username}):
+       #Get the credentials from Database
        authpass = userdata.find_one({"username": username})['password']
-       name = userdata.find_one({"username": username})['username']
-       name = userdata.find_one({"username": username})['username']
        salt = userdata.find_one({"username": username})['salt']
+       #Generate new token
        token = Cookiegen(50)
        hashpass = hashlib.sha256((pw+salt).encode('utf-8')).hexdigest()
        auth = hashlib.sha256((token).encode('utf-8')).hexdigest()
+       #Check PW
        if authpass == hashpass:
-        userdata.update_one({"username": username},{"$set": {"aut_token": auth}})
-        response = make_response(render_template('battle.html', UserName = name))
+        #Update auth token
+        userdata.update_one({"username": username},{"$set": {"auth_token": auth}})
+        response = make_response(redirect('/battle'))
         response.set_cookie('auth', token, httponly=True, max_age=7200)
         return response
        else:
-           return render_template('index.html', LoginMessage="Password or Username Incorrect")
+           return redirect(f'/?LoginMessage={"Password or Username Incorrect"}')
     else:
-        return render_template('index.html', LoginMessage="Password or Username Incorrect")
+        return redirect(f'/?LoginMessage={"Password or Username Incorrect"}')
 
 #LogOut
 @app.route("/logout", methods=['POST'])
 def Logout():
-    if 'auth' in request.cookies:
-        response = render_template('index.html', LogOutMessage="You haven't logged in")
-        return response
+    if 'auth' not in request.cookies:
+        return redirect('/')
     else:
-        response = make_response(render_template('index.html', LogOutMessage="Logged Out"))
+        response = redirect(f'/?LogOutMessage={"Logged Out Successful"}')
         response.set_cookie('auth', '', expires=0)
         return response
-    
+  
 @app.route("/battle/logout", methods=['POST'])
 def BattleLogOUt():
-    response = make_response(render_template('index.html', LogOutMessage="Logged Out"))
+    response = redirect(f'/?LogOutMessage={"Logged Out Successful"}')
     response.set_cookie('auth', '', expires=0)
     return response
 
